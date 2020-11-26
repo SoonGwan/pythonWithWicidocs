39a15a55e9df840ced1b6a9b43eafe9c5daf882b
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-26 19:31:05 +0900

    Create PyKoSpacing.py

---
+++ TextProcessingTools/PyKoSpacing.py
@@ -0,0 +1,7 @@
+from pykospacing import spacing
+sent = '김철수는 극중 두 인격의 사나이 이광수 역을 맡았다. 철수는 한국 유일의 태권도 전승자를 가리는 결전의 날을 앞두고 10년간 함께 훈련한 사형인 유연재(김광수 분)를 찾으러 속세로 내려온 인물이다.'
+
+new_sent = sent.replace(' ', '')
+
+kospacing_sent = spacing(new_sent)
+print(kospacing_sent)

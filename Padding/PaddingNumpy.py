461e1edeb89508b786873d0f116a4e17ee6779e5
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-12-01 11:09:31 +0900

    Update PaddingNumpy.py

--- Padding/PaddingNumpy.py
+++ Padding/PaddingNumpy.py
@@ -1,21 +1,25 @@
-import numpy as np
-from tensorflow.keras.preprocessing.text import Tokenizer
+# import numpy as np
+# from tensorflow.keras.preprocessing.text import Tokenizer
 
-sentences = [['barber', 'person'], ['barber', 'good', 'person'], ['barber', 'huge', 'person'], ['knew', 'secret'], ['secret', 'kept', 'huge', 'secret'], ['huge', 'secret'], [
-    'barber', 'kept', 'word'], ['barber', 'kept', 'word'], ['barber', 'kept', 'secret'], ['keeping', 'keeping', 'huge', 'secret', 'driving', 'barber', 'crazy'], ['barber', 'went', 'huge', 'mountain']]
+# sentences = [['barber', 'person'], ['barber', 'good', 'person'], ['barber', 'huge', 'person'], ['knew', 'secret'], ['secret', 'kept', 'huge', 'secret'], ['huge', 'secret'], [
+#     'barber', 'kept', 'word'], ['barber', 'kept', 'word'], ['barber', 'kept', 'secret'], ['keeping', 'keeping', 'huge', 'secret', 'driving', 'barber', 'crazy'], ['barber', 'went', 'huge', 'mountain']]
 
-tokenizer = Tokenizer()
-# fit_on_texts() 안에 코퍼스를 이별긍로하면 빈 도수를 기준으로 단어 집합을 생성
-tokenizer.fit_on_texts(sentences)
+# tokenizer = Tokenizer()
+# # fit_on_texts() 안에 코퍼스를 이별긍로하면 빈 도수를 기준으로 단어 집합을 생성
+# tokenizer.fit_on_texts(sentences)
 
-encoded = tokenizer.texts_to_sequences(sentences)
-print(encoded)
+# encoded = tokenizer.texts_to_sequences(sentences)
+# print(encoded)
 
-max_len = max(len(item) for item in encoded)
-print(max_len)
+# max_len = max(len(item) for item in encoded)
+# print(max_len)
 
-for item in encoded:
-    while len(item) < max_len:
-        item.append(0)
-padded_np = np.array(encoded)
-print(padded_np)
+# for item in encoded:
+#     while len(item) < max_len:
+#         item.append(0)
+# padded_np = np.array(encoded)
+# print(padded_np)
+
+
+a = [0, 0, 1, 0, 0, 0]
+print(a.index(1))
